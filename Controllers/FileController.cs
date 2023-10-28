917700c662934610da9cd49a3975e8594e442135
Vitalik769 <93341791+Vitalik769@users.noreply.github.com>
2023-10-28 20:08:58 +0300

    Add files via upload

---
+++ Controllers/FileController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace AspNetMVC.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public FileController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DownloadFile(string firstName, string lastName, string fileName)
+        {
+            if (!fileName.EndsWith(".txt"))
+            {
+                fileName += ".txt";
+            }
+
+            var filePath = Path.Combine(_hostingEnvironment.WebRootPath, "downloads", fileName);
+
+            var fileContent = $"Ім'я: {firstName}{Environment.NewLine}Прізвище: {lastName}";
+
+            await System.IO.File.WriteAllTextAsync(filePath, fileContent, Encoding.UTF8);
+
+            return PhysicalFile(filePath, "text/plain", fileName);
+        }
+
+    }
+}
