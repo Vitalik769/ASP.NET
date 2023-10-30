19df473ecd60058d527d1a1b5c3a313bc971bf91
Vitalik769 <93341791+Vitalik769@users.noreply.github.com>
2023-10-30 16:57:03 +0200

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using AspNetMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var product = new List<Product>
+            {
+                new Product { ID = 1, Name = "Audi Q7 2017", Price = 54.777, CreatedDate = DateTime.Now },
+                new Product { ID = 2, Name = "BMW X5 2015", Price = 38.500, CreatedDate = DateTime.Now.AddMinutes(-15) },
+                new Product { ID = 3, Name = "Mercedes-Benz C-Class 2016", Price = 27.999, CreatedDate = DateTime.Now.AddDays(-2) }
+            };
+            return View(product);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,28 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.Run(async context =>
+if (app.Environment.IsDevelopment())
 {
-    //1
-    Company company = new Company();
-    company.Name = "Apple";
-    company.Email = "Apple@gmail.com";
-    company.Address = "Infinite Loop Cupertino, California 95014";
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
-    //2
-    Random random = new Random();
-    int randomNumber = random.Next(0, 101);
-    //Îutput
-    await context.Response.WriteAsync($"Compamy:\n {company.Name} \n {company.Email} \n {company.Address} ");
-    await context.Response.WriteAsync($"\nRandom number: {randomNumber}");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "product",
+        pattern: "{controller=Product}/{action=Index}");
 
 });
+
 app.Run();
-public class Company
-{
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Address { get; set; }
 
-
-}
